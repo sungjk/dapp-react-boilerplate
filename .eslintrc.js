43bb8a8e5d98d81b8aceba7e777f61fac4bf5d9b
sungjk <ajax0615@gmail.com>
2018-06-13 00:50:03 +0900

    Initial commit 🚀

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,19 +11,16 @@ const fbjs = require('eslint-config-fbjs');
 
 module.exports = {
     extends: 'fbjs',
-    plugins: [...fbjs.plugins, 'header', 'prettier'],
+    plugins: [...fbjs.plugins],
     rules: {
         // disable rules from eslint-config-fbjs
         'react/react-in-jsx-scope': 0, // not needed with our metro implementation
         'no-new': 0, // new keyword needed e.g. new Notification
         'no-catch-shadow': 0, // only relevant for IE8 and below
         'no-bitwise': 0, // bitwise operations needed in some places
+        'no-console': 'off',
         'consistent-return': 0,
         'max-len': 0, // let's take prettier take care of this
         indent: 0, // let's take prettier take care of this
-        
-        // additional rules for this project
-        //'header/header': [2, 'block'],
-        //'prettier/prettier': [2, 'fb', '@format'],
     },
 };
